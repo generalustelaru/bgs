1f125026f748be5451d6f5a836061210c7830df1
generalustelaru <adrianpope@protonmail.com>
2020-01-29 21:46:36 +0200

    Add window tab icon. Create Subscription element.

--- scripts/validations.js
+++ scripts/validations.js
@@ -19,5 +19,20 @@ function contactForm(){
   } else {
     document.getElementById('emailError').innerHTML = "Oops! Something's wrong with your e-mail address :(";
     document.getElementById('email').style.border = "1px solid red";
+    //document.getElementById('email').style.padding.left = "10px;";
+  }
+}
+function subscribe(){
+  var eMail = document.getElementById('email').value;
+  if (eMail.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    alert("You're all set!");
+    document.getElementById('emailError').innerHTML = "";
+    document.getElementById('email').style.border = "";
+    document.getElementById('email').value = "";
+  }else{
+    document.getElementById('emailError').innerHTML = "<p>Oops! Something's wrong!</p>";
+    document.getElementById('email').style.border = "1px solid red";
+    document.getElementById('email').style.height = "2em";
+    //document.getElementById('email').style.width = "310px";
   }
 }
